9feaf24933a83fdf2875264707c76eb13b460a58
miguel <miguelcrako72@gmail.com>
2021-06-23 19:24:25 -0500

    Terminando las funciones

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,33 +7,30 @@ function cambuarFondo (){
     let btn_texto = document.querySelectorAll('.boton');
     let btn_fondo = document.querySelectorAll('.boton-bg');
     let contenido = document.querySelectorAll('.contenido-principal');
-
+    let contenido_texto = document.querySelectorAll('.contenido-principal .contenedor');
+   
     btn_texto.forEach( boton => {
         boton.addEventListener('click', function(){
-            let claseRemover = contenido[0].classList[2];
             let clase = boton.id;
             
-            contenido[0].classList.remove(claseRemover);
-            contenido[0].classList.add(clase)
+            contenido_texto[0].id = clase;
 
-            console.log(contenido[0].classList)
+            console.log(contenido_texto[0])
         })
    });
 
-   /*
+   
    btn_fondo.forEach( boton => {
         boton.addEventListener('click', function(){
-            let claseRemover = contenido[0].classList[2];
-            let clase = boton.id;
+           let clase = boton.id;
             
-            contenido[0].classList.remove(claseRemover);
-            contenido[0].classList.add(clase)
+            contenido[0].id = clase;
 
-            console.log(contenido[0].classList)
+            console.log(contenido[0])
         })
     });
 
-    */
+    
         
     //});
 }
